cfdf0c3201f2fa0dcd1826badfee71ec45d72ac7
Danny Forster <danny.forster@condenast.de>
2016-01-15 15:18:36 +0100

    VID-203: When no element is selected in the relation list, the field don't freeze anymore.

--- assets/RelationList.js
+++ assets/RelationList.js
@@ -1,14 +1,14 @@
 
 /**
-  * @typedef RelationListConfig
-  * @type Object
-  * @property {String} baseUrl - Needed to construct full URLs for REST calls
-  * @property {String} contenttype - Contenttype of the current content object. Used to retrieve the field configrations within the backend
-  * @property {String} fieldName - Name of the field that is currently being edited. Used to retrieve the field configrations within the backend
-  * @property {Object} validation - Holds all validation parameters
-  * @property {String} validation.min - Minimum number of selected elements
-  * @property {String} validation.max - Maximum number of selected elements
-  */
+ * @typedef RelationListConfig
+ * @type Object
+ * @property {String} baseUrl - Needed to construct full URLs for REST calls
+ * @property {String} contenttype - Contenttype of the current content object. Used to retrieve the field configrations within the backend
+ * @property {String} fieldName - Name of the field that is currently being edited. Used to retrieve the field configrations within the backend
+ * @property {Object} validation - Holds all validation parameters
+ * @property {String} validation.min - Minimum number of selected elements
+ * @property {String} validation.max - Maximum number of selected elements
+ */
 
 /**
  * Constructor
@@ -19,19 +19,19 @@ var RelationListComponent = function( config )
 	var self = this;
 
 	/**
-	  *	Holds all necessary fields that needs to
-	  * be accessed accross the component
-	  */
+	 *	Holds all necessary fields that needs to
+	 * be accessed accross the component
+	 */
 	self.config = config || {};
 
 	/**
-	  * List of selected content objects
-	  */
+	 * List of selected content objects
+	 */
 	self.selectedElements = [];
 
 	/**
-	  * Api URLs to be used
-	  */
+	 * Api URLs to be used
+	 */
 	self.apiUrls = {
 		findEntries: "/relationlist/finditems/##contenttype##/##field##/##search##",
 		fetchJsonList: "/relationlist/fetchJsonList"
@@ -43,94 +43,94 @@ var RelationListComponent = function( config )
 		console.warn("[RelationList::fetchJsonElements] self.config.baseUrl is not defined! This may lead to invalid API calls on different environments!");
 
 	/**
-	  * Initialize node elements
-	  */
+	 * Initialize node elements
+	 */
 	self.config.storageFieldNode = $("#" + self.config.fieldName),
-	self.config.componentContainerNode = self.config.storageFieldNode.closest(".RelationListComponent");
+		self.config.componentContainerNode = self.config.storageFieldNode.closest(".RelationListComponent");
 	self.config.searchFieldNode = $("#search-" + self.config.fieldName),
-	self.config.outputContainerNode = $("#searchResult-" + self.config.fieldName),
-	self.config.selectedElementsNode = $("#selectedElements-" + self.config.fieldName),
+		self.config.outputContainerNode = $("#searchResult-" + self.config.fieldName),
+		self.config.selectedElementsNode = $("#selectedElements-" + self.config.fieldName),
 
-	self.config.outputContainerNode.hide();
+		self.config.outputContainerNode.hide();
 
 	/**
-	  * HTML templates to be compiled
-	  * using the RelationList.applyVariables method
-	  */
+	 * HTML templates to be compiled
+	 * using the RelationList.applyVariables method
+	 */
 	self.searchResultEntryTemplate = "<div class=\"entry\">"
-										+ "<a class=\"title\" data-datechanged=\"##datechanged##\" data-contenttype=\"##contenttype##\" data-entry-id=\"##id##\" data-entry-title=\"##title##\" data-entry-slug=\"##slug##\" href=\"#\">##title##</a>"
-										+ "<span class=\"type\">##contenttype##</span>"
-										+ "<span class=\"date\">##datechanged##</span>"
-									+ "</div>";
+		+ "<a class=\"title\" data-datechanged=\"##datechanged##\" data-contenttype=\"##contenttype##\" data-entry-id=\"##id##\" data-entry-title=\"##title##\" data-entry-slug=\"##slug##\" href=\"#\">##title##</a>"
+		+ "<span class=\"type\">##contenttype##</span>"
+		+ "<span class=\"date\">##datechanged##</span>"
+		+ "</div>";
 
 	self.selectedEntryTemplate = "<div class=\"entry\" data-entry-id=\"##id##\">"
-									+ "<a target=\"_blank\" class=\"title\" data-entry-id=\"##id##\" href=\"##link##\">##title##</a>"
-									+ "<span class=\"type\">##contenttype##</span>"
-									+ "<span class=\"date\">##datechanged##</span>"
-									+ "<a href=\"#\" class=\"remove fa fa-trash\">Remove</a>"
-								+ "</div>";
+		+ "<a target=\"_blank\" class=\"title\" data-entry-id=\"##id##\" href=\"##link##\">##title##</a>"
+		+ "<span class=\"type\">##contenttype##</span>"
+		+ "<span class=\"date\">##datechanged##</span>"
+		+ "<a href=\"#\" class=\"remove fa fa-trash\">Remove</a>"
+		+ "</div>";
 
 	/**
-	  * Keeps the currently processed AJAX search request
-	  */
+	 * Keeps the currently processed AJAX search request
+	 */
 	self.searchRequest = {};
 
 	/**
-	  * Private methods
-	  */
+	 * Private methods
+	 */
 
 	/**
 	 * Check if variable is a number
-	 * 
+	 *
 	 * @param  {[type]} n - Variable to be parsed
 	 * @return {Boolean}
 	 */
 	self.isNumeric = function( n ) {
-	  return !isNaN( parseFloat( n ) ) && isFinite(n);
+		return !isNaN( parseFloat( n ) ) && isFinite(n);
 	}
 
 
 	/**
-	  * Compile HTML markup templates
-	  *
-	  * @param {String} template - Hold HTML markup with placeholders in the format `##key##`. `key` gets replaced by data[key]
-	  * @param {Object} data - Holds the variables necessary to process the markup
-	  *
-	  * @return {String} Compiled HTML
-	  */
+	 * Compile HTML markup templates
+	 *
+	 * @param {String} template - Hold HTML markup with placeholders in the format `##key##`. `key` gets replaced by data[key]
+	 * @param {Object} data - Holds the variables necessary to process the markup
+	 *
+	 * @return {String} Compiled HTML
+	 */
 	self.applyVariables = function( template, data ) {
-	    var result = template;
-	    var expr = /\#\#([a-z0-9_]*)\#\#/;
-	    var newResult = result;
+		var result = template;
+		var expr = /\#\#([a-z0-9_]*)\#\#/;
+		var newResult = result;
 
-	    do {
-	      result = newResult;
-	      newResult = result.replace( expr, function parsePlaceholders( match, placeholder ){
-	          return ( typeof data[placeholder] === "string" ) ? data[placeholder] : "undefined";
-	      });
-	    } while ( result !== newResult );
+		do {
+			result = newResult;
+			newResult = result.replace( expr, function parsePlaceholders( match, placeholder ){
+				return ( typeof data[placeholder] === "string" ) ? data[placeholder] : "undefined";
+			});
+		} while ( result !== newResult );
 
-	    return result;
+		return result;
 	};
 
 	/**
-	  * Print an info text, that there are no elements selected
-	  */
+	 * Print an info text, that there are no elements selected
+	 */
 	self.setNothingSelected = function( ) {
 		self.config.selectedElementsNode.html("<span class=\"message\">Nothing selected...</span>")
 	};
 
 	/**
-	  * Callback method that removes the clicked content element
-	  * from the `selected elements` list
-	  * 
-	  * Method requires context! (In case, use `apply`)
-	  */
+	 * Callback method that removes the clicked content element
+	 * from the `selected elements` list
+	 *
+	 * Method requires context! (In case, use `apply`)
+	 */
 	self.removeEntry = function( ) {
 		// Do validation beforehand!
 		if ( self.config.validation
-				&& typeof self.config.validation.min === "string"
-				&& self.isNumeric(self.config.validation.min) ) {
+			&& typeof self.config.validation.min === "string"
+			&& self.isNumeric(self.config.validation.min) ) {
 			if ( self.selectedElements.length <= self.config.validation.min ) {
 				alert("At least " + self.config.validation.min + " element(s) have to be selected!" );
 				return;
@@ -157,16 +157,16 @@ var RelationListComponent = function( config )
 	};
 
 	/**
-	  * Generate an absolute API url based on the self.config.baseUrl setting.
-	  *
-	  * @param {String} relativeUrl - Url with leading slash `/`
-	  *
-	  * @return {String} Absolute URL to the API service
-	  */
+	 * Generate an absolute API url based on the self.config.baseUrl setting.
+	 *
+	 * @param {String} relativeUrl - Url with leading slash `/`
+	 *
+	 * @return {String} Absolute URL to the API service
+	 */
 	self.getAbsoluteApiUrl = function( relativeUrl ) {
 		if ( typeof self.config.baseUrl !== "string"
-				|| typeof relativeUrl !== 'string'
-				|| relativeUrl.length === 0 )
+			|| typeof relativeUrl !== 'string'
+			|| relativeUrl.length === 0 )
 			return relativeUrl;
 
 		var baseUrl = self.config.baseUrl
@@ -175,21 +175,21 @@ var RelationListComponent = function( config )
 		// Remove trailing slash
 		if ( lastBaseChar === "/" || lastBaseChar === "\\" )
 			baseUrl = baseUrl.substr( 0, baseUrl.length-1 )
-		
+
 		return ( baseUrl + relativeUrl );
 	};
 
 	/**
-	  * Callback method that adds the clicked content element
-	  * to the `selected elements` list
-	  * 
-	  * Method requires context! (In case, use `apply`)
-	  */
+	 * Callback method that adds the clicked content element
+	 * to the `selected elements` list
+	 *
+	 * Method requires context! (In case, use `apply`)
+	 */
 	self.addEntry = function( ) {
 		// Do validation beforehand
 		if ( self.config.validation
-				&& typeof self.config.validation.max === "string"
-				&& self.isNumeric(self.config.validation.max) ) {
+			&& typeof self.config.validation.max === "string"
+			&& self.isNumeric(self.config.validation.max) ) {
 			if ( self.selectedElements.length >= self.config.validation.max ) {
 				alert("A maximum of " + self.config.validation.max + " element(s) can be selected!" );
 				self.destroySearchProcess();
@@ -226,9 +226,9 @@ var RelationListComponent = function( config )
 	};
 
 	/**
-	  * Adds the clicked content elements from
-	  * the search list to the `selected elements` list
-	  */
+	 * Adds the clicked content elements from
+	 * the search list to the `selected elements` list
+	 */
 	self.registerClickListeners = function( ) {
 		self.config.outputContainerNode.find('.entry a').mousedown(function( ) {
 			self.addEntry.apply(this);
@@ -238,38 +238,40 @@ var RelationListComponent = function( config )
 
 
 	/**
-	  * In case of an internal component error, the component
-	  * should be freezed so that the data consistency can
-	  * be obtained
-	  */
+	 * In case of an internal component error, the component
+	 * should be freezed so that the data consistency can
+	 * be obtained
+	 */
 	self.freeze = function ( ) {
 		self.config.searchFieldNode.prop('disabled', true);
 		self.config.componentContainerNode.addClass('error');
 	}
 
 	/**
-	  * Write current list into the hidden input field
-	  */
+	 * Write current list into the hidden input field
+	 */
 	self.storeSelectedList = function( ) {
 		var res = {};
 		var elements = self.selectedElements;
+		var jsonValue = '';
 
 		for ( var i=0; i < elements.length; i ++ )
 			res[i] = elements[i].id;
 
-		var jsonValue = JSON.stringify(res);
+		if(elements.length > 0)
+			jsonValue = JSON.stringify(res);
 
 		self.config.storageFieldNode.val(jsonValue);
 	};
 
 	/**
-	  * Send a JSON string with a list of IDs in form of `<contenttype>/<id>` to the API service
-	  * 
-	  * @param {String} jsonString - Holds the ID list
-	  * @param {Function} successCallback - Function to be called when finished processing. First parameter will be the data.
-	  * 
-	  * @return {undefined}
-	  */
+	 * Send a JSON string with a list of IDs in form of `<contenttype>/<id>` to the API service
+	 *
+	 * @param {String} jsonString - Holds the ID list
+	 * @param {Function} successCallback - Function to be called when finished processing. First parameter will be the data.
+	 *
+	 * @return {undefined}
+	 */
 	self.fetchJsonElements = function( jsonString, successCallback ) {
 		if ( typeof jsonString === "string" && jsonString.length === 0 )
 			return;
@@ -287,11 +289,11 @@ var RelationListComponent = function( config )
 		});
 	};
 
-    /**
-	  * Search function to find according entries
-	  * 
-	  * @param {String} keyword - Term to search for
-	  */
+	/**
+	 * Search function to find according entries
+	 *
+	 * @param {String} keyword - Term to search for
+	 */
 	self.findEntries = function ( keyword )
 	{
 		if ( typeof self.searchRequest === "object" && typeof self.searchRequest.abort === "function" ) {
@@ -300,11 +302,11 @@ var RelationListComponent = function( config )
 		}
 
 		var searchUrl = self.applyVariables(
-				self.getAbsoluteApiUrl( self.apiUrls.findEntries ), {
-					"contenttype": self.config.contenttype,
-					"field": self.config.fieldName,
-					"search": keyword
-				});
+			self.getAbsoluteApiUrl( self.apiUrls.findEntries ), {
+				"contenttype": self.config.contenttype,
+				"field": self.config.fieldName,
+				"search": keyword
+			});
 
 		self.searchRequest = $.ajax({
 			type: "GET",
@@ -354,15 +356,15 @@ var RelationListComponent = function( config )
 
 
 	/**
-	  * To be called, when search process
-	  * was cancelled. Removes the search result list
-	  * and aborts the search request, if running.
-	  */
+	 * To be called, when search process
+	 * was cancelled. Removes the search result list
+	 * and aborts the search request, if running.
+	 */
 	self.destroySearchProcess = function ( )
 	{
 		if ( self
-				&& typeof self.searchRequest === "object"
-				&& typeof self.searchRequest.abort === "function" ) {
+			&& typeof self.searchRequest === "object"
+			&& typeof self.searchRequest.abort === "function" ) {
 
 			self.searchRequest.abort();
 		}
@@ -373,21 +375,21 @@ var RelationListComponent = function( config )
 
 
 	/**
-	  * Initialize...
-	  */
+	 * Initialize...
+	 */
 	self.setNothingSelected();
 
 	/**
-	  * Add some UI interaction magic:
-	  * - Search field user interactions
-	  */
- 	self.config.searchFieldNode.on('keyup', function handleSearchInput( event ) {
+	 * Add some UI interaction magic:
+	 * - Search field user interactions
+	 */
+	self.config.searchFieldNode.on('keyup', function handleSearchInput( event ) {
 		if ( self.config.searchFieldNode.val().length > 2 ) {
 			self.findEntries( self.config.searchFieldNode.val(), "searchResult-{{key}}" );
 		}
 	}).on("blur", self.destroySearchProcess);
 
- 	self.config.searchFieldNode.val( self.initialKeyword );
+	self.config.searchFieldNode.val( self.initialKeyword );
 
 	self.config.searchFieldNode.on('focus', function( ) {
 		if ( self.config.searchFieldNode.val() === self.initialKeyword ) {
@@ -433,15 +435,15 @@ var RelationListComponent = function( config )
 
 
 	/**
-	  * Public methods
-	  */
+	 * Public methods
+	 */
 	return {
 
 		/**
-		  * Return the internal list of selected elements
-		  *
-		  * @return {Object} List of all selected elements
-		  */
+		 * Return the internal list of selected elements
+		 *
+		 * @return {Object} List of all selected elements
+		 */
 		getSelectedElements: function( ) {
 			return jQuery.extend( {}, self.selectedElements);
 		},
